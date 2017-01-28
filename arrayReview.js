15812e8e8c4fdb1c428f2c8bea50bedfc0c5a724
Rebekah Richardson <bekahrichardson0422@gmail.com>
2017-01-28 10:05:25 -0700

    vowels problem in array review

--- arrayReview.js
+++ arrayReview.js
@@ -109,3 +109,12 @@ var myPoem = 'What is a jQuery but a misunderstood object?'
 
 var theOdyssey = "function expression or function declaration? Tis an obvious choice";
 //Write a function called vowelCounter that takes in a string (theOdyssey) and returns how many vowels are in that string.
+function vowelCounter(str) {
+  vowels = 0;
+  for (var i = 0; i < str.length; i++) {
+    if (str.charAt(i) === 'a' || str.charAt(i) === 'e' || str.charAt(i) === 'i' || str.charAt(i) === 'o' || str.charAt(i) === 'u') {
+      vowels++;
+    }
+  };
+  return vowels;
+}
