2536cc0fb39faef0c6247fb299208d0379323e59
Rebekah Richardson <bekahrichardson0422@gmail.com>
2017-01-28 11:34:39 -0700

    Rest of the Constructor review

--- constructorReview.js
+++ constructorReview.js
@@ -42,18 +42,26 @@ Persons.push(new Person('Aaron', '23', '72' 'M'));
 
 //Now we want every instance of Animal to have a eat method. This method will choose a random item in that instances food array, then alert "(name) ' ate ' (whichever food was chosen)".
 
-  //code here
+  Animal.prototype.eat = function () {
+    var randomFood = Math.floor(Math.random() * this.food.length);
+    alert(this.name + ' ate ' + this.food[randomFood]);
+  }
 
 
 //At this point, if we wanted to add something to every istance of person could we?
 
   //Yes or no? and why or why not?
-
+// NO - Persons are just of the Object Class, and are not given their own class.
+// Because of this, we cannot run any prototype method on the Person class.
 
 
 /*
   1) What happens when you use the 'new' keyword to call a Constructor function?
+  Answer: A "new" object of that type is created.
   2) What's a good way to describe the keyword 'this'
+  Answer: It is a reference to the object that is calling the function.
   3) Can a normal function which creates an object and then returns that object use the prototype?
+  Answer: No, because the objects created are just under the Object class, and are not given their own class.
   4) What happens if you forget to use 'new' when calling a constructor?
+  Answer: If you forget, then you run the Constructor Function, but will not create any new object.
 */
